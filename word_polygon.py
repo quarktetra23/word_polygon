8e2cca36855dca4e4fcffec6ffcc2bca683942e4
Abhishek Manhas <147427209+quarktetra23@users.noreply.github.com>
2023-12-05 22:59:09 -0500

    Update word_polygon.py

--- word_polygon.py
+++ word_polygon.py
@@ -1,1 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+def draw_polygon(n_sides):
+    angles = np.linspace(0, 2 * np.pi, n_sides, endpoint=False)
+    x = np.cos(angles)
+    y = np.sin(angles)
+    return np.append(x, x[0]), np.append(y, y[0])  # Append the first point to the end to close the loop
+
+def calculate_distance(point1, point2):
+    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def plot_word(word, x, y):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    letter_offset = 1.1  # Increase this value to move letters further out
+    positions = {letter: (x[i], y[i]) for i, letter in enumerate(alphabet)}
+
+    # Plot all letters around the polygon with an offset
+    for i, letter in enumerate(alphabet):
+        offset_x, offset_y = x[i] * letter_offset, y[i] * letter_offset
+        plt.text(offset_x, offset_y, letter.upper(), fontsize=12, ha='center', va='center')
+
+    prev_char = word[0].lower()
+    plt.plot(x, y, marker='o')
+
+    total_length = 0
+    for char in word[1:].lower():
+        if char != prev_char:
+            plt.plot(*zip(*[positions[prev_char], positions[char]]), marker='o')
+            total_length += calculate_distance(positions[prev_char], positions[char])
+        prev_char = char
+
+    # Mark the origin with a unique color
+    plt.scatter(0, 0, color='magenta', marker='x')
+
+    plt.xlim(-1.5, 1.5)
+    plt.ylim(-1.5, 1.5)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.title(f"Total Length of Path: {total_length:.2f}")
+    plt.show()
+
+# Main
+n_sides = 26
+x, y = draw_polygon(n_sides)
+word = input("Enter a word: ")
+plot_word(word, x, y)
