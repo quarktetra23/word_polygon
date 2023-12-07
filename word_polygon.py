ca02fe7424c2879ea2c1d13830e6cc1b8dda5c37
Abhishek Manhas <147427209+quarktetra23@users.noreply.github.com>
2023-12-06 20:06:36 -0500

    Update word_polygon.py
    addition of the shoelace formula to calculate the area generated of the complex polygon within the circle

--- word_polygon.py
+++ word_polygon.py
@@ -10,6 +10,11 @@ def draw_polygon(n_sides):
 def calculate_distance(point1, point2):
     return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
+def shoelace_area(coords):
+    x = coords[:, 0]
+    y = coords[:, 1]
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
 def plot_word(word, x, y):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     letter_offset = 1.1  # Increase this value to move letters further out
@@ -24,11 +29,19 @@ def plot_word(word, x, y):
     plt.plot(x, y, marker='o')
 
     total_length = 0
-    for char in word[1:].lower():
+    word_coords = []
+    for char in word.lower():
         if char != prev_char:
             plt.plot(*zip(*[positions[prev_char], positions[char]]), marker='o')
             total_length += calculate_distance(positions[prev_char], positions[char])
+            word_coords.append(positions[char])
         prev_char = char
+
+    # Calculate the area if the word forms a closed shape
+    if len(word_coords) > 2:
+        enclosed_area = shoelace_area(np.array(word_coords))
+    else:
+        enclosed_area = 0
 
     # Mark the origin with a unique color
     plt.scatter(0, 0, color='magenta', marker='x')
@@ -36,7 +49,7 @@ def plot_word(word, x, y):
     plt.xlim(-1.5, 1.5)
     plt.ylim(-1.5, 1.5)
     plt.gca().set_aspect('equal', adjustable='box')
-    plt.title(f"Total Length of Path: {total_length:.2f}")
+    plt.title(f"Total Length of Path: {total_length:.2f}, Enclosed Area: {enclosed_area:.2f}")
     plt.show()
 
 # Main
